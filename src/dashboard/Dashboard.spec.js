efa446317a0a1c0c77c3b640c4998c8ac95b1bda
nevets615 <surj316@yahoo.com>
2019-05-15 18:59:46 -0400

    pull request

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
-// Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+
+import Dashboard from "./Dashboard";
+
+it('renders component successfully', () => {
+    render(<Dashboard />);
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,33 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import renderer from "react-test-renderer";
+import Display from "./Display";
+
+describe("display", () => {
+  it("renders component successfully", () => {
+    render(<Display />);
+  });
+  it("check if led is green on open", () => {
+    console.log(Display.defaultProps["closed"]);
+    const { getByText, container } = render(<Display />);
+    if (Display.defaultProps["closed"] === false) {
+      const expected = getByText("Open");
+      console.log(expected);
+
+      expect(container.firstChild).toHaveClass(/green-led/i);
+    }
+  });
+  it("check if led is red on close", () => {
+    const { getByText, container } = render(<Display />);
+    if (Display.defaultProps["locked"] === false) {
+      const expected = getByText("Unlocked");
+      expect(container.firstChild).toHaveClass("green-led");
+    }
+  });
+  it("matches component snapshot", () => {
+    const snap = renderer.create(<Display />);
+    expect(snap.toJSON()).toMatchSnapshot();
+  });
+});
